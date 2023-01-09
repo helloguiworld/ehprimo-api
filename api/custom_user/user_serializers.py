d9bb4bd2aa7277af7355873b91f8a5584e573f91
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2023-01-09 03:03:49 -0300

    fix use of set_password in user serializer

--- api/custom_user/user_serializers.py
+++ api/custom_user/user_serializers.py
@@ -6,3 +6,10 @@ class CustomUserSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
